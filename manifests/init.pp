b1bb7f4bcd83e37e8f2eb857c5766d91106f11b2
akomakom <akomakom@users.noreply.github.com>
2018-09-06 13:09:05 -0400

    Multiple instances configurable, puppet 5 support (#2)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,35 @@
-class socat ( $param='' ) {
-	package { 'socat':
-		ensure => installed,
-	}
+# params is a hash of socat instances to run
+#  {
+#    'name1' => 'command line args',
+#    'name2' => 'command line args',
+#  }
+class socat ( $params={} ) {
+  package { 'socat':
+    ensure => installed,
+  }
 
-	file { '/etc/init.d/socat':
-		source => 'puppet:///modules/socat/socat',
-		require => Package['socat'],
-		notify  => Service['socat'],
-		owner   => root, group => root, mode => 0750;
-	}
+  File{
+    owner => 'root',
+    group => 'root',
+  }
 
-	file { '/etc/default/socat':
-		owner   => root,
-		group   => root,
-		mode    => 664,
-		content => template('socat/socat.erb'),
-	}
+  file { '/etc/init.d/socat':
+    source => 'puppet:///modules/socat/socat',
+    require => Package['socat'],
+    notify  => Service['socat'],
+    mode => "0750";
+  }
 
-	service { 'socat':
-		ensure    => running,
-		enable    => true,
-		hasstatus => true,
-		require   => Package['socat'],
-	}
+  file { '/etc/default/socat':
+    mode    => "664",
+    content => template('socat/socat.erb'),
+    notify  => Service['socat'],
+  }
+
+  service { 'socat':
+    ensure    => running,
+    enable    => true,
+    hasstatus => true,
+    require   => Package['socat'],
+  }
 }
